61d0a0e92f8997cb68cc99c3d9a57344f9c54837
raghav888 <raghavbang2012@gmail.com>
2025-10-14 20:15:40 +0530

    connection pagination api

--- src/routes/user.js
+++ src/routes/user.js
@@ -59,6 +59,7 @@ router.get("/connections", userAuth, async (req, res) => {
     }
 });
 
+// /feed?page=1&limit=10
 // return all user from db - IMP API
 router.get("/feed", userAuth, async (req, res) => {
     try {
@@ -72,17 +73,29 @@ router.get("/feed", userAuth, async (req, res) => {
         // find all connection requests (sent + recevied)
         const connectionRequests = await ConnectionRequest.find({
             $or: [{ requestFromId: id }, { requestToId: id }],
-        }).select(["requestFromId", "requestToId"]); // it selects only those field from doc 
+        }).select(["requestFromId", "requestToId"]); // it selects only those field from doc
 
         const connectedIds = new Set();
 
-        connectionRequests.forEach(req => {
-            connectedIds.add(req.requestFromId);
-            connectedIds.add(req.requestToId);
-        })
+        connectionRequests.forEach((req) => {
+            connectedIds.add(req.requestFromId.toString());
+            connectedIds.add(req.requestToId.toString());
+        });
 
-        // get all data from User collection where ids are not equal to any user id.
-        const feeds = await User.find({ _id: { $nin: Array.from(connectedIds) } }).select(USER_SAFE_DATA);
+        // get query values and convert to number as they are string
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+
+        if (limit > 20) {
+            return res.status(400).json({ message: "Wrong query values" })
+        }
+        // get all data from User collection where any user id is not equal to any user id from connectionIds.
+        // and also curr user profile should not be there so _id not equal to curr user id
+        const feeds = await User.find({ _id: { $nin: [...connectedIds, id] } })
+            .skip((page - 1) * limit) // skip initial n records
+            .limit(limit) // return n to limit records only
+            .select(USER_SAFE_DATA);
+
         res.status(200).json({ message: "feeds", data: feeds });
     } catch (err) {
         res.status(500).send("Error while retriving all data" + err.message);
@@ -90,3 +103,7 @@ router.get("/feed", userAuth, async (req, res) => {
 });
 
 module.exports = router;
+
+
+// when filtering, sorting, pagination -> use query
+// when entity finding, or find resource like id, etc -> use param
