1123f5d5e07983c4642a5263cb8b28c544a63d6c
raghav888 <raghavbang2012@gmail.com>
2025-11-15 22:02:31 +0530

    Remove hardcoded database connection string and database name from configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
 
-const CLUSTER_CONNECTION_STRING = 'mongodb+srv://raghavbang_1:Sowupu%40123@cluster-node.4izim8n.mongodb.net/';
-const DB_NAME = "devTinder"
-
 // this returns a promise, so should be inside async await
 const connectDB = async () => {
     // TAKES Cluster/DBName
